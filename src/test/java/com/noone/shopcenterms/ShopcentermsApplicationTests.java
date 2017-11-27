046e31bd1193f8aff4d496aa775a89189cb98463
JackLiao <jackliao@edayun.cn>
2017-11-27 17:53:49 +0800

    shopcenterms inital

---
+++ src/test/java/com/noone/shopcenterms/ShopcentermsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.noone.shopcenterms;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShopcentermsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
